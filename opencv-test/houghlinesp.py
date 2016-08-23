d5d708d84cb2c74260c6a1f46ede5f12679889ee
Emerson Cloud <emerson@localhost.localdomain>
2016-08-23 15:44:49 -0400

    Added some mods to houghTransform.py

--- opencv-test/houghlinesp.py
+++ opencv-test/houghlinesp.py
@@ -4,7 +4,7 @@ import numpy as np
 img = cv2.imread('test.jpg')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,150,apertureSize = 3)
-minLineLength = 1000
+minLineLength = 100
 maxLineGap = 10
 lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
 for x1,y1,x2,y2 in lines[0]:
